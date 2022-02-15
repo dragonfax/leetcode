8b4c763990f150010c1aa33d1942a438b933e488
Jason Stillwell <>
2022-02-15 13:22:38 +0000

    single number

--- main.py
+++ main.py
@@ -1,39 +1,27 @@
 from typing import Optional, List
 from time import perf_counter
+from collections import defaultdict
 
 
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-      largestSubLen = 0
-
-      sub = {}
-      for i in range(0,len(s)):
-        a = s[i]
-        if a not in sub:
-          sub[a] = i
-          subLen = len(sub)
-          if subLen > largestSubLen:
-            largestSubLen = subLen
+    def singleNumber(self, nums: List[int]) -> int:
+      a = {}
+      for n in nums:
+        if n in a:
+          del a[n]
         else:
-          # remove that element, and all the ones before it. continue
-          ai = sub[a]
-          sub[a] = i
-          for b in list(sub):
-            bi = sub[b]
-            if bi < ai:
-              del sub[b]
+          a[n] = True
+      return list(a.keys())[0]
 
-      return largestSubLen
+
 
 def test(s, e):
   start = perf_counter()
-  a = Solution().lengthOfLongestSubstring(s)
+  a = Solution().singleNumber(s)
   end = perf_counter()
   print(f"{s}=> {a} == {a == e} {end - start}")
 
-test("abcabcbb",3)
-test("bbbbbbbb",1)
-test("pwwkew",3)
-test("aab",2)
+test([2,2,1],1)
+test([4,1,2,1,2],4)
+test([1],1)
 
-test("abcdefghijklmnopqrstufwxyz0123456789abcdefghijklmnopqrstufwxyz0123456789abcdefghijklmnopqrstufwxyz0123456789abcdefghijklmnopqrstufwxyz0123456789",35)
